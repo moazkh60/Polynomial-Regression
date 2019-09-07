f5a48e8514890c2354914d174b346f294baaccec
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-09-07 23:18:46 +0500

    Visualization and Prediction using polynomial regression

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -39,3 +39,26 @@ X_poly = poly_reg.fit_transform(X)
 lin_reg2 = LinearRegression()
 lin_reg2.fit(X_poly, y) 
 
+# Visualizing Linear Regression
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color='green')
+
+# Visualizing Polynomial Regression
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X,y, color='red')
+plt.plot(X_grid, lin_reg2.predict(poly_reg.fit_transform(X_grid)), color='blue')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+
+# Predicting the salary
+lin_reg2.predict(poly_reg.fit_transform(6.5))
+
+
+
+
+
+
+
+
+
