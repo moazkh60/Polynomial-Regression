1ead20a6373a1f108ac04e0585793d1770965df0
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-09-07 22:51:34 +0500

    linear regression then polynomial variables setup

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -13,16 +13,29 @@ import pandas as pd
 
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:,:-1].values
-y = dataset.iloc[:, 3].values
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
 
 # Splitting the data into training and test sets
-from sklearn.model_selection import train_test_split
+"""from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state = 0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScalar
+from sklearn.preprocessing import StandardScalar
 sc_X = StandardScalar()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
+
+# First linear regression
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,y)
+
+# Polynomial Regression
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly, y) 
+
