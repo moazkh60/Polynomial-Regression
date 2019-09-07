4884193a27fee4c2c7053f9ba42d93cdccc136ec
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-09-07 22:32:33 +0500

    Data and preprocessing template added

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep  7 22:25:01 2019
-
-@author: muhammadmoazkhan
-"""
+# Data Preprocessing
 
 # Importing the libraries
 import numpy as np
